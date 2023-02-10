b74510ebf96e27024f3210f5bfb78329761247a0
Abdulmumin <lanreywaju97@gmail.com>
2023-02-10 23:57:37 +0100

    wrote a test for the string representation of the Income model

--- tracker/tests.py
+++ tracker/tests.py
@@ -10,6 +10,9 @@ class TrackerModelTests(TestCase):
     def setUp(self):
         self.income = Income.objects.create(amount=3.33, source="Salary")
 
+    def test_string_representation(self):
+        self.assertEqual(str(self.income), "3.33 from Salary")
+
     def test_income_model(self):
         # income = Income.objects.create(amount=3.33, source="Salary")
         self.assertEqual(self.income.amount, 3.33)
