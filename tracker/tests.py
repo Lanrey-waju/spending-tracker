a53d629d0f171b5c537bef9c8d61f9db34d63714
Abdulmumin <lanreywaju97@gmail.com>
2023-02-11 23:49:43 +0100

    Wrote more test amd model code

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 from .models import Income
+
+User = get_user_model()
 
 # Create your tests here.
 class TrackerModelTests(TestCase):
@@ -8,10 +11,14 @@ class TrackerModelTests(TestCase):
     Tests the Income Model
     """
     def setUp(self):
+        self.user = User.objects.create_user(email="user@user.com", first_name="user", password="foo")
         self.income = Income.objects.create(amount=3.33, source="Salary")
 
     def test_string_representation(self):
         self.assertEqual(str(self.income), "3.33 from Salary")
+    
+    def test_user_can_create_income_instance(self):
+        self.assertEqual(self.income.user, self.user)
 
     def test_income_model(self):
         # income = Income.objects.create(amount=3.33, source="Salary")
