c379ce3e4c68b3b1e619337070eee5dbf6f0c536
Abdulmumin <lanreywaju97@gmail.com>
2023-02-13 23:48:41 +0100

    Added string representation for Expense mode

--- tracker/models.py
+++ tracker/models.py
@@ -27,3 +27,6 @@ class Expense(models.Model):
     category = models.CharField(max_length=10, choices=CATEGORIES, default=OTHER)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.amount} om {self.category}"
